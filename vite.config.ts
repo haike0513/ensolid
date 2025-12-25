6dc23e599245dbbe93e87875a94d35e0f9e7ea7e
haike0513 <haike0513@gmail.com>
2025-12-25 10:18:34 +0800

    Integrate Tailwind CSS into the project; update package.json and pnpm-lock.yaml to include Tailwind CSS and related dependencies, and modify Vite configuration to utilize Tailwind CSS plugin. Enhance index.css to import Tailwind CSS styles.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,18 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
-import path from 'path'
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+// import { fileURLToPath } from "url";
+
+// const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), tailwindcss()],
   resolve: {
     alias: {
-      '@resolid/baseui': path.resolve(__dirname, './packages/baseui/src'),
-      '@resolid/radix': path.resolve(__dirname, './packages/radix/src'),
-      '@': path.resolve(__dirname, './'),
+      "@resolid/baseui": path.resolve(__dirname, "./packages/baseui/src"),
+      "@resolid/radix": path.resolve(__dirname, "./packages/radix/src"),
+      "@": path.resolve(__dirname, "./"),
     },
   },
-})
+});
